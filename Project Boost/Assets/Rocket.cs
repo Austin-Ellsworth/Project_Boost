6f758520b8cc494d248265c1178d61892f360079
Austin Ellsworth <austin_ellsworth1@yahoo.com>
2020-01-14 20:21:08 -0500

    Revert "add boost keys"
    This reverts commit ae44ee3a76b8ef78eab20c2d01e8ae77ccc14b72.

--- Project Boost/Assets/Rocket.cs
+++ Project Boost/Assets/Rocket.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidBody;
-
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
@@ -19,18 +16,6 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rigidBody.AddRelativeForce(Vector3.up);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            print("Rotating Left");
-        } 
-        else if (Input.GetKey(KeyCode.D))
-        {
-            print("Rotating Right");
-        }
+        if()
     }
 }
